045e7550a414b7968878bac52e10e3dc5bc2d8e1
MukitHossen7 <hossenmukit7@gmail.com>
2025-08-25 00:25:40 +0600

    feat: implement Send Money page with form handling and UI components

--- src/routes/userSidebarItems.ts
+++ src/routes/userSidebarItems.ts
@@ -2,11 +2,11 @@ import { lazy } from "react";
 const DepositMoney = lazy(() => import("@/pages/user/DepositMoney"));
 const MyProfile = lazy(() => import("@/pages/user/MyProfile"));
 const Overview = lazy(() => import("@/pages/user/Overview"));
-const SendMoney = lazy(() => import("@/pages/user/SendMoney"));
+// const SendMoney = lazy(() => import("@/pages/user/SendMoney"));
 const TransactionHistory = lazy(
   () => import("@/pages/user/TransactionHistory")
 );
-const WithdrawMoney = lazy(() => import("@/pages/user/WithdrawMoney"));
+// const WithdrawMoney = lazy(() => import("@/pages/user/WithdrawMoney"));
 
 export const userSidebarItems = [
   {
@@ -19,27 +19,27 @@ export const userSidebarItems = [
         component: Overview,
       },
       {
-        title: "Deposit Money",
-        url: "/user/deposit",
+        title: "Wallet",
+        url: "/user/wallet",
         component: DepositMoney,
       },
-      {
-        title: "Withdraw Money",
-        url: "/user/withdraw",
-        component: WithdrawMoney,
-      },
-      {
-        title: "Send Money",
-        url: "/user/send-money",
-        component: SendMoney,
-      },
+      // {
+      //   title: "Withdraw Money",
+      //   url: "/user/withdraw",
+      //   component: WithdrawMoney,
+      // },
+      // {
+      //   title: "Send Money",
+      //   url: "/user/send-money",
+      //   component: SendMoney,
+      // },
       {
         title: "History",
         url: "/user/transactions-history",
         component: TransactionHistory,
       },
       {
-        title: "My Profile",
+        title: "Profile",
         url: "/user/profile",
         component: MyProfile,
       },
